3d238817d0cc6d9933c26216841d9c2b4d345f63
Serj-Y <newlife101101@gmail.com>
2023-04-19 11:47:39 +0300

    'master'

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -4,16 +4,15 @@ import * as axios from "axios";
 import userPhotoDefault from '../../../assets/img/4314581-200.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             });
     }
+
     render() {
         return <div>
-            <button onClick={this.getUsers} >getUsers</button>
             {
                 this.props.users.map(u => <div key={u.id}>
                     <span >

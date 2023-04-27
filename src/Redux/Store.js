1be63f1d40e2008f0c9c46b34f9d7f38b35be401
Serj-Y <newlife101101@gmail.com>
2023-04-27 15:32:20 +0300

    setState, local state
    ProfileStatus

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,13 +1,11 @@
-import profileReducer from './profileReducer';
-import dialogReducer from './dialogsReducer';
-import sidebarReducer from './sidebarReducer';
+
 
 let store = {
   _state: {
     profilePage: {
       posts: [
-        { id: 1, message: 'Sho ti', likesCount: 13 },
-        { id: 2, message: 'Zdorova zaibal', likesCount: 24 },
+        { id: 1, message: '', likesCount: 13 },
+        { id: 2, message: '', likesCount: 24 },
         { id: 3, message: 'post', likesCount: 83 },
         { id: 4, message: '(;', likesCount: 39 },
         { id: 5, message: '):', likesCount: 45 },
@@ -24,8 +22,8 @@ let store = {
       ],
 
       messages: [
-        { id: 1, message: 'Sho ti' },
-        { id: 2, message: 'Zdorova zaibal' },
+        { id: 1, message: '' },
+        { id: 2, message: 't' },
         { id: 3, message: 'Hi' },
         { id: 4, message: '(;' },
         { id: 5, message: '):' }
@@ -47,15 +45,3 @@ let store = {
   subscribe(observer) {
     this._callSubscriber = observer;
   },
-
-  dispatch(action) { //dispatch({type ''  action ???}) !!!
-    this._state.profilePage = profileReducer(this._state.profilePage, action);
-    this._state.messagesPage = dialogReducer(this._state.messagesPage, action);
-    this._state.sidebar = sidebarReducer(this._state.sidebar, action);
-    this._callSubscriber(this._state);
-  }
-};
-
-
-
-window.store = store;
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import s from "./ProfileInfo.module.css"
 import Preloader from "../../Common/Preloader/Preloader";
-
+import ProfileStatus from "./ProfileStatus"
 const ProfileInfo = (props) => {
 if (!props.profile) {
     return <Preloader/>
 }
-
-
-
     return <div>
-        <div>
-            <img src='https://cdn-imgix.headout.com/tour/29976/TOUR-IMAGE/f063b1b9-8a23-47ad-bc54-aaab71c5a7a0-a94e6f65-f5fd-4cf6-9b58-54676cfe5106-58e9748a-ed65-4e6b-93bb-e32bfd2340a8-16070-dubai-img-worlds-of-adventure-ticket---free-meal-voucher-08.jpeg'></img>
-        </div>
         <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large}></img>
-            Description?
+            <ProfileStatus status="Hi!"/>
             </div>
     </div>
 }

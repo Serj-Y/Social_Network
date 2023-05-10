f6a9e25ce0d5b5dda1243206e34202f3352199e7
Serj-Y <newlife101101@gmail.com>
2023-05-10 11:09:37 +0300

    __

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,24 +5,24 @@ import style from "./Nav.module.css";
 const Nav = React.memo(props => {
   return (
     <nav className={style.nav} >
-      <div >
+      <div className={style.item} >
         <div>
-          <NavLink to="/profile"  >Profile</NavLink>
+          <NavLink to="/profile" className={({ isActive }) => isActive ? style.active : style.link}> Profile</NavLink>
         </div>
         <div>
-          <NavLink to="/messages" >Messages</NavLink>
+          <NavLink to="/messages" className={({ isActive }) => isActive ? style.active : style.link} >Messages</NavLink>
         </div>
         <div>
-          <NavLink to="/users" >Users</NavLink>
+          <NavLink to="/users" className={({ isActive }) => isActive ? style.active : style.link}>Users</NavLink>
         </div>
         <div>
-          <NavLink to="/news" >News</NavLink>
+          <NavLink to="/news" className={({ isActive }) => isActive ? style.active : style.link}>News</NavLink>
         </div>
         <div>
-          <NavLink to="/music" >Music</NavLink>
+          <NavLink to="/music" className={({ isActive }) => isActive ? style.active : style.link} >Music</NavLink>
         </div>
         <div>
-          <NavLink to="/settings" >Settings</NavLink>
+          <NavLink to="/settings" className={({ isActive }) => isActive ? style.active : style.link}>Settings</NavLink>
         </div>
       </div>
     </nav>

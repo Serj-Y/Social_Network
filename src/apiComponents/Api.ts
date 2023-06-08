17dcb6fe8cbea123aaa23268e62ff495e60867bd
Serj-Y <newlife101101@gmail.com>
2023-06-08 14:50:23 +0300

    Update Api.ts

--- src/apiComponents/Api.ts
+++ src/apiComponents/Api.ts
@@ -4,5 +4,9 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: `https://social-network.samuraijs.com/api/1.0/`,
     withCredentials: true,
+    headers:     {
+        "API-KEY": "c45350d0-e2fd-4a10-9287-8f3cf738a929"
+    }
+    
   
 });

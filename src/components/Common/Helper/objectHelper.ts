8940b2d0029ebaa21862708b58a5a384634e09ba
Serj-Y <newlife101101@gmail.com>
2023-06-23 13:08:49 +0300

    Js To Ts

---
+++ src/components/Common/Helper/objectHelper.ts
@@ -0,0 +1,10 @@
+
+
+
+export const updateObjectInArray = (items: any[], itemId: number, objPropName: string, newObjProps: { followed: boolean; }) => {
+return items.map(u => {
+    if (u[objPropName] === itemId) {
+        return { ...u, ...newObjProps }
+    }
+    return u;
+})}
